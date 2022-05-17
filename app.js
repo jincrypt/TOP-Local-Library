c3a394777a6ca71859e207efd33978a5d4dcd443
jincrypt <leung.mka@gmail.com>
2022-05-17 18:10:20 -0400

    Created catalog route & controllers

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://TOP-Local-Library:bW50oNTeFv7rKFUU@cluster0.t2p2s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
+//Set up mongoose connection
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 var app = express();
 
@@ -21,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/catalog', catalogRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
